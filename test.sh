0a7f7052ada7bb3ae65644b13df9e54858259c9b
David van Buiten <david.vanbuiten@gmail.com>
2023-11-27 14:52:27 +0100

    Removed test.sh

--- test.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-set -u   # crash on missing env variables
-set -e   # stop on any error
-set -x   # print what we are doing
-
-curl -d '{"text":"poep op straat"}' -H "Content-Type: application/json" -X POST http://localhost:8140/signals_mltool/predict
